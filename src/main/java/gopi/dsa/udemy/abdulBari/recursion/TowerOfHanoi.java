fbf42fda86d705e92dafe2f1a3a579d0098f22ec
Gopinath <gopinathjnv@gmail.com>
2020-02-28 19:49:43 +0530

    Tower of Hanoi

---
+++ src/main/java/gopi/dsa/udemy/abdulBari/recursion/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+/**
+ * Created With Love By Gopi on 28-Feb-2020
+ */
+package gopi.dsa.udemy.abdulBari.recursion;
+
+/**
+ * @author Gopi
+ */
+public class TowerOfHanoi {
+	static int stepsCount = 0;
+
+	public static void towerOfHanoi(int n, String A, String B, String C) {
+
+		if (n > 0) {
+			towerOfHanoi(n - 1, A, C, B);
+			System.out.println("Moving disk " + n + " from " + A + " to " + C + " Step Number " + ++stepsCount);
+			towerOfHanoi(n - 1, B, A, C);
+		}
+	}
+
+	public static void main(String[] args) {
+		TowerOfHanoi.towerOfHanoi(4, "A", "B", "C");
+	}
+}
