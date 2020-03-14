302792769ff50a24cf5c11ad3a308164f2fdeac0
Gopinath <gopinathjnv@gmail.com>
2020-03-14 15:00:45 +0530

    LL operations

--- src/main/java/gopi/dsa/udemy/abdulBari/LinkedList/SinglyLinkedList.java
+++ src/main/java/gopi/dsa/udemy/abdulBari/LinkedList/SinglyLinkedList.java
@@ -6,17 +6,20 @@ package gopi.dsa.udemy.abdulBari.LinkedList;
 import java.awt.HeadlessException;
 import java.util.Arrays;
 
+import org.checkerframework.checker.units.qual.Temperature;
+
 /**
  * @author Gopi
  */
 public class SinglyLinkedList
 {
 	Node head;
+	Node last;
 
 	public static void main(String[] args)
 	{
 		SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-		singlyLinkedList.createLinkedList(1, 2, 3, 4, 5, 6);
+		singlyLinkedList.createLinkedList(1, 2, 3, 9, 5, 6);
 		singlyLinkedList.display();
 		System.out.println("Displaying Linked List elements Recursively... ");
 		singlyLinkedList.displayRecursively(singlyLinkedList.head);
@@ -25,8 +28,34 @@ public class SinglyLinkedList
 		singlyLinkedList.displayReverseRecursively(singlyLinkedList.head);
 
 		System.out.println("\nSum of Elements Data :" + singlyLinkedList.sumOfElementsData(singlyLinkedList.head));
-		
+
 		System.out.println("Number of Nodes in LL :" + singlyLinkedList.count(singlyLinkedList.head));
+
+		System.out.println("Max element in the Linked list is : " + singlyLinkedList.max(singlyLinkedList.head));
+		singlyLinkedList.display();
+		singlyLinkedList.insert(20, 0);
+		singlyLinkedList.insert(30, 4);
+		singlyLinkedList.insert(50, 9);
+		singlyLinkedList.display();
+		SinglyLinkedList singlyLinkedList2 = new SinglyLinkedList();
+		singlyLinkedList2.insertLast(60);
+		singlyLinkedList2.insertLast(70);
+		singlyLinkedList2.insertLast(90);
+		singlyLinkedList2.display();
+
+		SinglyLinkedList singlyLinkedList3 = new SinglyLinkedList();
+		singlyLinkedList3.insertInSortedList(30);
+		singlyLinkedList3.insertInSortedList(20);
+		singlyLinkedList3.insertInSortedList(10);
+		singlyLinkedList3.insertInSortedList(40);
+		singlyLinkedList3.insertInSortedList(25);
+		singlyLinkedList3.display();
+
+		singlyLinkedList3.delete(0);
+		singlyLinkedList3.delete(1);
+		singlyLinkedList3.delete(2);
+		singlyLinkedList3.delete(0);
+		singlyLinkedList3.delete(4);
 	}
 
 	/**
@@ -61,7 +90,7 @@ public class SinglyLinkedList
 
 	public void display()
 	{
-		System.out.print("Displaying Elements : ");
+		System.out.print("\nDisplaying Elements : ");
 		Node temp = head;
 		while (null != temp)
 		{
@@ -69,6 +98,108 @@ public class SinglyLinkedList
 			temp = temp.next;
 
 		}
+	}
+
+	public void insert(int data, int pos)
+	{
+		Node node = new Node(data, null);
+
+		if (pos == 0 || head == null)
+		{
+			node.next = head;
+			head = node;
+		} else if (pos > 0)
+		{
+			Node temp = head;
+			for (int i = 0; i < pos - 1 && temp.next != null; i++)
+			{
+				temp = temp.next;
+			}
+			node.next = temp.next;
+			temp.next = node;
+		}
+	}
+
+	public void insertLast(int data)
+	{
+		Node node = new Node(data, null);
+
+		if (head == null)
+		{
+			last = head = node;
+		} else
+		{
+			last.next = node;
+			last = node;
+		}
+	}
+
+	public void insertInSortedList(int data)
+	{
+		Node node = new Node(data, null);
+
+		if (head == null)
+		{
+			last = head = node;
+		} else
+		{
+
+			Node next = head;
+			Node prev = null;
+			if (head.data > data)
+			{
+				node.next = head;
+				head = node;
+				return;
+			}
+			while (next != null && next.data < data)
+			{
+				prev = next;
+				next = next.next;
+
+			}
+			node.next = prev.next;
+			prev.next = node;
+		}
+	}
+
+	public void delete(int pos)
+	{
+		if (head != null)
+		{
+
+			if (pos == 0)
+			{
+				Node temp = head;
+				head = head.next;
+				temp = null;
+
+			} else
+			{
+
+				Node next = head;
+				Node prev = null;
+				int count = 0;
+				while (next != null && pos > count++)
+				{
+					prev = next;
+					next = next.next;
+
+				}
+				if (next != null)
+				{
+					prev.next = next.next;
+					next.next = null;
+				}
+				else
+				{
+					System.out.print("\nAs position given :"+pos+" doesn't exist in the List");
+					display();
+				}
+			}
+		}
+		System.out.print("\nAfter Deleting element at pos: " + pos + " from List: ");
+		display();
 	}
 
 	public void displayRecursively(Node temp)
@@ -121,6 +252,27 @@ public class SinglyLinkedList
 		return (1 + count(head.next));
 
 	}
+
+	public int max(Node head)
+	{
+		int max = Integer.MIN_VALUE;
+		if (null == head)
+		{
+			return max;
+		} else
+		{
+			max = max(head.next);
+			if (max > head.data)
+			{
+				return max;
+			} else
+			{
+
+				return head.data;
+			}
+		}
+	}
+
 }
 
 class Node
